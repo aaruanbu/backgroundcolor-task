fc38a77f1c478ee6e66af373e735cde7dcbd1648
aaruanbu <aaruanbu001@gmail.com>
2023-07-15 11:49:07 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ let body=document.getElementById("body")
+  
+    
+	 function change(){
+	 let invalue=document.getElementById("bgcolor").value;
+	 document.getElementById("out")
+	 out.innerHTML=invalue;
+	
+	 body.style.background=invalue;
+	 }
